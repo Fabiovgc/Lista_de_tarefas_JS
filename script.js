cf6cea7f64a6606795d8deba5ec403b8fe2c13b9
FabioVGC <fabiovgc@gmail.com>
2025-01-08 16:22:52 -0300

    feat: add função para adicionar tarefa ; botão de remover tarefa ; Imagem para fundo da página ;

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const taskInp = document.getElementById('taskInput');
-const taskLis = document.getElementById('taskList');
+const input = document.getElementById('taskInput');
+const ul = document.getElementById('taskList');
 
-// function addTask(){
-    
-// }
+function addTask(){
+    if(input.value != ""){
+
+        // Criamos uma constante "li" que armazena o elemento lista
+        // Definimos que o valor da lista será o mesmo que for digitado no input.
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        // Criamos uma constante "removeButton" que tem o 'PODER' de criar um elemento Botão.
+        // Definimos que o conteúdo de texto dentro do botão, se for criado, será "Remover".
+        // Definimos um evento de clique ligado ao botão que chamará uma função que tem a capacidade de remover a lista criada
+        const removeButton = document.createElement('button');
+        removeButton.textContent = "Remover";
+        removeButton.onclick = function(){
+            ul.removeChild(li);
+        };
+
+        // Adicionamos o botão à lista, se essa for criada
+        // Adicionamos uma lista ao elemento ul, quando isso for ordenado pela lógica do programa
+        // Chamamos uma mensagem "Insira uma tarefa" se uma tarefa sem texto tentar ser adicionada
+        li.appendChild(removeButton);
+        ul.appendChild(li);
+
+        input.value = ""
+    } else{
+        alert('Insira uma tarefa!')
+    }
+}
